e56fd6eaf7e73a707c7c74674c581269286d8699
Julian Ostmo <julian.ostmo@ibm.com>
2023-06-07 15:55:26 +0100

    fix: fixed eslint error

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const config = require('config');
 const app = require('./src/index');
-const logger = require('./src/utils/logger');
 
 const port = config.get('app.port');
 
 app.listen(port, () => {
-  logger.info(`App listening on port:${port}`);
+  console.info(`App listening on port:${port}`);
 });
