8d1236fad9fe711025b4304744e15cc72d0d5bda
Julian Ostmo <julian.ostmo@ibm.com>
2023-06-21 14:12:11 +0100

    chore: removed old mq config

--- config/default.js
+++ config/default.js
@@ -7,11 +7,4 @@ module.exports = {
   logger: {
     level: 'info',
   },
-  mq: {
-    host: 'https://web-test-mq-manager-ceb3.qm2.us-south.mq.appdomain.cloud/',
-    username: process.env.MQ_USERNAME,
-    apiKey: process.env.API_KEY,
-    queue: 'DEV.QUEUE.1',
-    queueManager: 'test_mq_manager',
-  },
 };
