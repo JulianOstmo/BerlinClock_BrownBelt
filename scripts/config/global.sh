ee4b088a3670dc34c041340c61972afae4aeb2df
Julian Ostmo <julian.ostmo@ibm.com>
2023-07-19 12:56:21 +0100

    fix: corrected deployment names

--- scripts/config/global.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IBMCLOUD_API=https://cloud.ibm.com
-IBMCLOUD_RESOURCE_GROUP=Default
-REGISTRY_URL="us.icr.io"
-REGION="us-south"
-REGISTRY_TAGS_AND_NAMESPACES=("latest:xpfarm")
-
-# Comes through from travis secure environment variables
-IBMCLOUD_APIKEY=$SECURE_IBMCLOUD_APIKEY
